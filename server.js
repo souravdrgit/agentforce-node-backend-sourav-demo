7d6a4f7cddad7e89f0f21114c3773000dd43c491
SouravDR <drsourav4u@gmail.com>
2026-05-28 13:31:29 +0530

    Fixed Render dynamic port

--- server.js
+++ server.js
@@ -194,7 +194,13 @@ app.post("/upload-and-analyze", upload.single("image"), async (req, res) => {
 });
 
 
-// 🔹 START SERVER
-app.listen(5000, "0.0.0.0", () => {
+// 🔹 START SERVER--5000 block works fine for local
+/*app.listen(5000, "0.0.0.0", () => {
     console.log("Server running at http://localhost:5000");
+}*/
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
+);
